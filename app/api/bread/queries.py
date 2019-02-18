fcf0af046b2f88dec1f5ca0a62a18239abeb31c5
Simon Bos <simonbos9@gmail.com>
2019-02-18 16:03:05 +0100

    Implemented global actions for bread

--- app/api/bread/queries.py
+++ app/api/bread/queries.py
@@ -1,14 +1,33 @@
+from app import db
 from app.models import BreadOrder, BreadOrderDate
 
 
-def get_all_order_dates(user, after_date):
+def get_order_dates(after_date):
+    """
+    Returns the order dates occurring after the given date.
+    """
+    query = BreadOrderDate.query \
+                          .filter(BreadOrderDate.date > after_date)
+    return query.all()
+
+
+def get_orders(user, after_date):
+    """
+    Returns all orders for the given user occurring after the given date.
+    """
+    query = BreadOrder.query \
+                      .join(BreadOrderDate) \
+                      .filter(BreadOrderDate.date > after_date) \
+                      .filter(BreadOrder.user_id == user.id)
+    return query.all()
+
+
+def get_order_dates_extended(user, after_date):
     """
     Returns for all order dates the orders of a user after a given date.
     """
     result = {}
-    dates = BreadOrderDate.query \
-                          .filter(BreadOrderDate.date > after_date) \
-                          .all()
+    dates = get_order_dates(after_date)
     for order_date in dates:
         result[order_date.id] = {
             'id': order_date.id,
@@ -19,18 +38,72 @@ def get_all_order_dates(user, after_date):
             'total_price': 0
         }
 
-    items = BreadOrder.query \
-                      .join(BreadOrderDate) \
-                      .filter(BreadOrderDate.date > after_date) \
-                      .filter(BreadOrder.user_id == user.id) \
-                      .all()
-    for item in items:
-        if item.date.id not in result:
+    orders = get_orders(user, after_date)
+    for order in orders:
+        if order.date.id not in result:
             continue
-        data = result[item.date.id]
+        data = result[order.date.id]
         data['orders'].append({
-            'id': item.id,
-            'type': item.type.name
+            'id': order.id,
+            'type': order.type.name
         })
-        data['total_price'] += item.type.price
+        data['total_price'] += order.type.price
     return result.values()
+
+
+def add_orders_on(user, order_date, items):
+    """
+    Adds for a user all orders specified by items on the specified date.
+    """
+    for item in items:
+        order = BreadOrder(
+            user=user,
+            date=order_date,
+            type=item
+        )
+        db.session.add(order)
+    db.session.commit()
+
+
+def add_orders_after(user, after_date, items):
+    """
+    Adds for a user all orders specified by items on all editable order dates
+    after the specified date.
+    """
+    order_dates = get_order_dates(after_date)
+    for order_date in order_dates:
+        if not order_date.is_editable:
+            continue
+
+        for item in items:
+            order = BreadOrder(
+                user=user,
+                date=order_date,
+                type=item
+            )
+            db.session.add(order)
+    db.session.commit()
+
+
+def delete_orders_on(user, order_date):
+    """
+    Deletes for a user all orders on the specified date.
+    """
+    db.session.query(BreadOrder).filter(
+        BreadOrder.user_id == user.id,
+        BreadOrder.date_id == order_date.id
+    ).delete()
+    db.session.commit()
+
+
+def delete_orders_after(user, after_date):
+    """
+    Deletes for a user all editable orders after the specified date.
+    """
+    orders = get_orders(user, after_date)
+    for order in orders:
+        if not order.date.is_editable:
+            continue
+
+        db.session.delete(order)
+    db.session.commit()
--- app/api/bread/views.py
+++ app/api/bread/views.py
@@ -4,10 +4,9 @@ import flask_jwt_extended as jwt
 from flask import request
 from flask_restful import Resource
 
-from app import db
 from app.api import api
-from app.models import BreadOrder, BreadType, BreadOrderDate
-from .queries import get_all_order_dates
+from app.models import BreadType, BreadOrderDate
+from . import queries
 from .schema import BreadOrderDates, BreadOrderingSchema, BreadTypeSchema
 
 bread_order_dates_schema = BreadOrderDates(many=True)
@@ -15,21 +14,24 @@ bread_ordering_schema = BreadOrderingSchema()
 bread_types_schema = BreadTypeSchema(many=True)
 
 
+def get_start_date():
+    # TODO: solve start_date hack
+    start_date = datetime.date.today()
+    if start_date.month >= 9:
+        start_date = start_date.replace(month=9, day=1)
+    else:
+        start_date = start_date.replace(month=1, day=1)
+    return start_date
+
+
 @api.resource('/bread/')
 class BreadOrderDateListResource(Resource):
 
     @jwt.jwt_required
     def get(self):
-        # TODO: solve start_date hack
-        start_date = datetime.date.today()
-        if start_date.month >= 9:
-            start_date = start_date.replace(month=9, day=1)
-        else:
-            start_date = start_date.replace(month=1, day=1)
-
         user = jwt.current_user
-        order_dates = get_all_order_dates(user, start_date)
-
+        start_date = get_start_date()
+        order_dates = queries.get_order_dates_extended(user, start_date)
         data, _ = bread_order_dates_schema.dump(order_dates)
         return {'success': True, 'order_dates': data}
 
@@ -51,14 +53,7 @@ class BreadOrderDateResource(Resource):
             return {'msg': '400 Bad Request', 'errors': errors}, 400
 
         user = jwt.current_user
-        for item in data.get('items'):
-            order = BreadOrder(
-                user=user,
-                date=order_date,
-                type=item
-            )
-            db.session.add(order)
-        db.session.commit()
+        queries.add_orders_on(user, order_date, data.get('items'))
         return {'success': True}
 
     @jwt.jwt_required
@@ -70,11 +65,30 @@ class BreadOrderDateResource(Resource):
             return {'msg': 'Order date not editable'}, 400
 
         user = jwt.current_user
-        db.session.query(BreadOrder).filter(
-            BreadOrder.user_id == user.id,
-            BreadOrder.date_id == order_date_id
-        ).delete()
-        db.session.commit()
+        queries.delete_orders_on(user, order_date)
+        return {'success': True}
+
+
+@api.resource('/bread/all')
+class BreadAllOrderDatesResource(Resource):
+
+    @jwt.jwt_required
+    def patch(self):
+        json_data = request.get_json()
+        data, errors = bread_ordering_schema.load(json_data)
+        if not data or errors:
+            return {'msg': '400 Bad Request', 'errors': errors}, 400
+
+        user = jwt.current_user
+        start_date = get_start_date()
+        queries.add_orders_after(user, start_date, data.get('items'))
+        return {'success': True}
+
+    @jwt.jwt_required
+    def delete(self):
+        user = jwt.current_user
+        start_date = get_start_date()
+        queries.delete_orders_after(user, start_date)
         return {'success': True}
 
 
